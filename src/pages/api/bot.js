4cdf4f9af1d543d1fc45e66598813d42330e0b52
andjanuardi <andjanuardi@gmail.com>
2023-09-17 22:27:25 +0700

    Bot 3

--- src/pages/api/bot.js
+++ src/pages/api/bot.js
@@ -1,10 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log({
-    req_method: req.method,
-    req_query: req.query,
-    req_body: req.body,
-  });
+  console.log(
+    JSON.stringify(
+      {
+        req_method: req.method,
+        req_query: req.query,
+        req_body: req.body,
+      },
+      " ",
+      1
+    )
+  );
   res.status(200).json("test");
 }
