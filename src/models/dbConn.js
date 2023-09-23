9cc2ec4f2df5f9ce557eef8ef2e74bd46c9b9633
Andri Januardi <67629644+andjanuardi@users.noreply.github.com>
2023-09-23 19:59:32 +0700

    Update dbConn.js
    Change Connection Limit

--- src/models/dbConn.js
+++ src/models/dbConn.js
@@ -1,6 +1,7 @@
 import mysql from "mysql";
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
+connectionLimit:100,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
